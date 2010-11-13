52b7a91a5a295501fbd4c1a8574db8216631bd2a
ade.baeyee <ade.baeyee@409dd048-5041-c712-048a-a0113b7fa627>
2010-11-13 07:33:12 +0000

    initial commit delete & update account

---
+++ application/controllers/account/delete_controller.php
@@ -0,0 +1,48 @@
+<?php
+
+class Delete_controller extends Controller {
+
+	function Delete_controller()
+	{
+		parent::Controller();	
+		$this->load->library('input');
+		$this->load->model('account_model');
+	}
+	
+	function index()
+	{
+		$this->session->set_userdata('current_menu','ACCOUNT');
+		$h_data['style']="simpel-herbal.css";
+		$m_data['notification_message']="";
+		$f_data['author']="ade";
+		$this->load->view('admin/header.php',$h_data);
+		$this->load->view('account/delete.php',$m_data);
+		$this->load->view('admin/footer.php',$f_data);
+	}
+	
+	function deleteAccount(){
+	//TODO delee
+		$this->form_validation->set_rules('username','Username','required');
+		$this->form_validation->set_rules('password','Password','required');
+		
+		if ($this->form_validation->run()==FALSE){
+			$m_data['notification_message']="Input Invalid";
+		}
+		else{
+			$username=$this->input->get_post('username');
+			$password=$this->input->get_post('password');
+			$role_name=$this->input->get_post('jabatan');
+			$this->account_model->create_account($username,SHA1($password),$role_name);
+			$m_data['notification_message']="Account successfully created";
+		}			
+		$h_data['style']="simpel-herbal.css";
+		$f_data['author']="ade";
+		$this->load->view('admin/header.php',$h_data);
+		$this->load->view('account/add.php',$m_data);
+		$this->load->view('admin/footer.php',$f_data);
+	}
+}
+
+/* End of file welcome.php */
+/* Location: ./system/application/controllers/welcome.php */
+?>
---
+++ application/views/account/update.php
@@ -0,0 +1,93 @@
+<html>
+<body>
+<div id="main">
+
+
+<div id="modulmenu">
+	<?php if( $this->session->userdata('current_modulmenu') == 'REGISTER' ) $type = 'current_modulmenu'; else $type = 'modulmenu';?>
+	<div class="<?php echo $type?>"><a href="<?=base_url()?>index.php/account/add_controller"> ADD</a></div> 
+	<?php if( $this->session->userdata('current_modulmenu') == 'PROFILE' ) $type = 'current_modulmenu'; else $type = 'modulmenu';?>
+	<div class="<?php echo $type?>"><a href="<?=base_url()?>index.php/account/search_controller"> SEARCH</a></div>
+	<?php if( $this->session->userdata('current_modulmenu') == 'PROFILE' ) $type = 'current_modulmenu'; else $type = 'modulmenu';?>
+	<div class="<?php echo $type?>"><a href="<?=base_url()?>index.php/account/update_controller"> UPDATE</a></div>
+	<?php if( $this->session->userdata('current_modulmenu') == 'PROFILE' ) $type = 'current_modulmenu'; else $type = 'modulmenu';?>
+	<div class="<?php echo $type?>"><a href="<?=base_url()?>index.php/account/delete_controller"> DELETE</a></div>
+</div>
+	
+<div class="modulheader">
+UPDATE
+</div>
+<div class="content">
+<div class="notification">
+<?php echo $notification_message;?>
+</div>
+<fieldset>
+<legend>Account Information</legend>
+
+<?php 
+	$attributes = array('id' => 'register_form');
+	$field1 = array( 
+	'type' => 'text',  
+	'name' => 'username',
+	'id' => 'username',
+	'rules' => 'required',
+	);
+	$field2 = array(
+	'type' => 'password',  
+	'name' => 'password',
+	'id' => 'password',
+	'rules' => 'required',
+	);
+	$field3 = array(
+	'type' => 'text',  
+	'name' => 'name',
+	'id' => 'name',
+	);
+	$field4 = array(
+	'type' => 'text',  
+	'name' => 'address',
+	'id' => 'address',
+	);
+	$field5 = array(
+	'pegawai_toko' => 'Pegawai',  
+	'pemilik_toko' => 'Pemilik',
+	'pengelola_toko' => 'Pengelola',
+	);
+	$field6 = array(
+	'type' => 'text',  
+	'name' => 'telp',
+	'id' => 'telp',
+	);
+
+echo form_open('account/add_controller/addAccount',$attributes);?>
+<table id="form">
+	<tr>
+	<td>Username</td><td>: <?php echo form_input($field1); ?></td>
+	</tr>
+	<tr>
+	<td>Password</td><td>: <?php echo form_input($field2); ?></td>
+	</tr>
+	<tr>
+	<td>Nama</td><td>: <?php echo form_input($field3); ?></td>
+	</tr>
+	<tr>
+	<td>Alamat</td><td>: <?php echo form_input($field4); ?></td>
+	</tr>
+	<tr>
+	<td>Jabatan</td><td>: <?php echo form_dropdown('jabatan',$field5,'pegawai_toko'); ?></td>
+	</tr>
+	<tr>
+	<td>No telp</td><td>: <?php echo form_input($field6); ?></td>
+	</tr>
+	<tr>
+	<td><?php echo form_submit('register', 'Register');?></td>
+	</tr>
+</table>
+<?php echo form_close();?>
+</div>
+
+</fieldset>
+</div>
+</div>
+</body>
+</html>
