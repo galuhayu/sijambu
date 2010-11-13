6f551412398337e783849a087b278aafcd4c5e2b
ade.baeyee <ade.baeyee@409dd048-5041-c712-048a-a0113b7fa627>
2010-11-13 06:53:41 +0000

    add account menu add & search

--- application/controllers/account/search_controller.php
+++ application/controllers/account/search_controller.php
@@ -5,15 +5,46 @@ class Search_controller extends Controller {
 	function Search_controller()
 	{
 		parent::Controller();	
+		$this->load->library('input');
+		$this->load->model('account_model');
 	}
 	
 	function index()
 	{
 		$this->session->set_userdata('current_menu','ACCOUNT');
 		$h_data['style']="simpel-herbal.css";
+		$m_data['notification_message'] = "";
+		$m_data['content'] = "";
 		$f_data['author']="ade";
 		$this->load->view('admin/header.php',$h_data);
-		$this->load->view('account/search.php');
+		$this->load->view('account/search.php',$m_data);
+		$this->load->view('admin/footer.php',$f_data);
+	}
+	
+	function searchAccount(){
+		$m_data['notification_message']="";
+		$m_data['content'] = "";
+		
+		$this->form_validation->set_rules('username','Username','required');
+		if ($this->form_validation->run()==FALSE){
+			$m_data['notification_message']="Enter Username To Search";
+		}
+		else{
+			$username=$this->input->get_post('username');
+			$data = $this->account_model->search_account($username);
+			if ($data !=0 ){
+				foreach ($data as $userdata) :
+					$m_data['content'] = $userdata;
+				endforeach;
+			}
+			else{
+				$m_data['notification_message']="User Not Found";
+			}
+		}
+		$h_data['style']="simpel-herbal.css";
+		$f_data['author']="ade";
+		$this->load->view('admin/header.php',$h_data);
+		$this->load->view('account/search.php',$m_data);
 		$this->load->view('admin/footer.php',$f_data);
 	}
 }
