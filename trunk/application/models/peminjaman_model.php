0deb5e972f089145f7bc0ef473c0433684c4c034
ade.baeyee <ade.baeyee@409dd048-5041-c712-048a-a0113b7fa627>
2010-12-05 04:15:49 +0000

    update peminjaman function

--- trunk/application/models/peminjaman_model.php
+++ trunk/application/models/peminjaman_model.php
@@ -11,14 +11,18 @@ class Peminjaman_model extends Model
 	}
 	
 	function add_line($idbuku){
-		$data = $this->db->query(" SELECT idbuku,namabuku,pengarang,hargasewa,lama FROM buku WHERE idbuku = $idbuku ");
+		$data = $this->db->query(" SELECT idbuku,namabuku,pengarang,hargasewa,lama FROM buku WHERE idbuku = $idbuku AND status = 1");
 		if ($data->num_rows!=0){
 			return $data->result_array();
 		}
 		else{
 			return 0;
-		}
-		
+		}		
+	}
+	
+	function save_line_transaction($idmember,$idbuku,$tipe,$tglpinjam,$tglkembali,$harga){
+		$data=$this->db->query("INSERT INTO transaksi (idmember,idbuku,tipepinjam,tglpinjam,tglkembali,harga)VALUES ($idmember,$idbuku,$tipe,'$tglpinjam','$tglkembali',$harga)");
+		$data = $this->db->query("UPDATE buku SET status = 0 WHERE idbuku = $idbuku");
 	}
 	
 }
