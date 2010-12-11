7791d4ae5ef0ac1688869e0dd752cb9b8ac21d8b
ade.baeyee <ade.baeyee@409dd048-5041-c712-048a-a0113b7fa627>
2010-12-11 04:58:33 +0000

    update header dan footer

--- trunk/application/views/admin/footer.php
+++ trunk/application/views/admin/footer.php
@@ -2,10 +2,6 @@
 
 	<div id="footer">
         <div class="info">
-        <?php 
-        if( $this->session->userdata('logged_in') == TRUE ) {?>
-        <a href="<?=base_url()?>index.php/logout_controller"> logout</a>
-        <?php }?>
         </div>
         <div class="author">Created by: <?php echo $author;?></div>
 	</div>
